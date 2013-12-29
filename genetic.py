fa42146a56e4a7b16b7db0c65fd7ddd9fe6a2e9b
pastuszka <pastuszka@pastuszka-laptop.(none)>
2013-12-29 12:19:33 +0100

    genetic algorithm finished

--- genetic.py
+++ genetic.py
@@ -4,13 +4,58 @@ Created on 27-12-2013
 @author: pastuszka
 '''
 
-from random import shuffle, randrange
+from random import shuffle, randrange, sample, random
+from makespan import makespan
 
 def genetic(times, population_size, num_of_epochs):
     job_count = len(times)
     population = [list(range(1, job_count + 1)) for _ in range(0, population_size)]
     for individual in population:
         shuffle(individual)
+        
+    population_with_fitness = evaluate_fitness(population, times)    
+    for _ in xrange(0, num_of_epochs):
+        parents = choose_parents(population_with_fitness)
+        children = breed(parents)
+        mutate(children)
+        population_with_fitness = merge(population_with_fitness, evaluate_fitness(children, times))
+    return choose_best(population_with_fitness)[0]
+
+def evaluate_fitness(population, times):
+    return [(individual, makespan(individual, times)) for individual in population]
+
+def choose_parents(population):
+    parents = []
+    for _ in xrange(0, len(population)):
+        tournament = sample(population, 5)
+        parents.append(choose_best(tournament))
+        
+    return parents
+
+def breed(parents):
+    shuffle(parents)
+    children = []
+    for i in range(1, len(parents), 2):
+        children += pmx(parents[i - 1][0], parents[i][0])
+    return children
+
+def mutate(children):
+    for child in children:
+        if random() > 0.6:
+            left = randrange(0, len(child))
+            right = randrange(left, len(child))
+           
+            tmp = child[left]
+            child[left] = child[right]
+            child[right] = tmp   
+
+def merge(parents, children):
+    both = parents + children
+    both.sort(key=lambda x: x[1])
+    return both[:len(parents)]
+
+def choose_best(population):
+    return min(population, key=lambda x: x[1])
 
 def translate(x, d):
     while x in d and x != d[x]:
@@ -30,10 +75,8 @@ def pmx(a, b):
     child1 = map(lambda x: translate(x, d_inv), a[0:left]) + b2 + map(lambda x: translate(x, d_inv), a[right:length + 1])
     child2 = map(lambda x: translate(x, d), b[0:left]) + a2 + map(lambda x: translate(x, d), b[right:length + 1])
     
-    return [child1, child2]
-
-print pmx([1, 2, 3, 4, 5, 6, 7, 8, 9], [5, 4, 6, 9, 2, 1, 7, 8, 3])        
+    return [child1, child2]     
     
-    
+   
         
     
--- tester.py
+++ tester.py
@@ -6,11 +6,12 @@ Created on 27-12-2013
 
 from makespan import makespan
 from cds import cds
+from genetic import genetic
 
 for i in range(1,17):
     with open('testcases/bat' + str(i)) as f:
         testcase = eval(f.read())
         
         print "Test case ", i
-        best_perm = cds(testcase)
-        print "CDS result: ", makespan(best_perm, testcase)
+        print "CDS result: ", makespan(cds(testcase), testcase)
+        print "genetic result: ", makespan(genetic(testcase, 100, 100), testcase)
